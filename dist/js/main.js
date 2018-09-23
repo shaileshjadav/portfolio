16203425a98f83a4bcd9078d4c810d1041c69e64
shaileshjadav <shaileshjadav007@gmail.com>
2018-09-23 15:51:54 +0530

    finished portfolio

--- dist/js/main.js
+++ dist/js/main.js
@@ -30,3 +30,81 @@ function ToggleMenu() {
         showMenu = false;
     }
 }
+
+
+const words = ["shailesh Jadav", "Webdevloper", "writes code"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+
+    var loopTyping = function () {
+        if (word.length > 0) {
+            if (document.getElementById('demo') != null) {
+                document.getElementById('demo').innerHTML += word.shift();
+            }
+        } else {
+            deletingEffect();
+            return false;
+        };
+
+        timer = setTimeout(loopTyping, 500);
+    };
+    loopTyping();
+};
+
+
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function () {
+        if (i % 2 === 1) {
+            document.getElementById('am').style.display = 'none';
+        }
+        else {
+
+            document.getElementById('am').style.display = 'inline-block';
+
+        }
+
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('demo').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            typingEffect();
+            return false;
+        };
+
+        timer = setTimeout(loopDeleting, 200);
+    };
+    loopDeleting();
+};
+
+typingEffect();
+
+
+if ((document.querySelector('.html') != null) && (document.querySelector('.css') != null) && (document.querySelector('.js') != null) && (document.querySelector('.ps') != null)) {
+    document.querySelector('.html').style.width = '90%';
+    document.querySelector('.css').style.width = '90%';
+    document.querySelector('.js').style.width = '70%';
+    document.querySelector('.ps').style.width = '85%';
+}
+
+// document.querySelector('.projects img').onmouseover(function () {
+//     alert("fsdf");
+// })
+    // $('.html').css('width', '90%');
+    // $('.css').css('width', '90%');
+    // $('.jquery').css('width', '70%');
+    // $('.javascript').css('width', '60%');
+    // $('.adobe').css('width', '80%');
+    // $('.wordpress').css('width', '10%');
+    // $('.ui').css('width', '70%');
+    // $('.rwd').css('width', '80%');
+
